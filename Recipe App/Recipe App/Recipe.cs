af57f1c6f48c5a52b299130ca1001cbbf8e2d919
Wahina Chabalala <st10310050@rcconnect.edu.za>
2024-04-11 21:01:53 +0200

    Added Ingredient and Step arrays
    Added Ingredient and Step arrays to the Recipe class in order to store the different ingredients and steps that the recipe consists of.

--- Recipe App/Recipe App/Recipe.cs
+++ Recipe App/Recipe App/Recipe.cs
@@ -2,5 +2,19 @@
 {
     internal class Recipe
     {
+        private Ingredient[] ingredients;
+        private Step[] steps;
+
+        public Ingredient[] Ingredients
+        {
+            get { return ingredients; }
+            set { ingredients = value; }
+        }
+
+        public Step[] Steps
+        {
+            get { return steps; }
+            set { steps = value; }
+        }
     }
 }
