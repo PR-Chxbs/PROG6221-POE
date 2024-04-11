26c94f88c9612fc333ea96b8f691114b59a7ba08
Wahina Chabalala <st10310050@rcconnect.edu.za>
2024-04-12 01:04:40 +0200

    Added VerifyInput for stricter input handling
    Added a new method called VerifyInput that will put the input through stricter conditions. Such as a number being in between two specified number

--- Recipe App/Recipe App/Program.cs
+++ Recipe App/Recipe App/Program.cs
@@ -4,6 +4,8 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        Recipe recipe;
+
         string menu = 
             "Select an option:" + "\n" +
             "1. Add Recipe Details" + "\n" +
@@ -14,7 +16,51 @@ internal class Program
             "6. Quit" + "\n" +
             ": "; 
 
-        int choice = GetIntInput(menu);
+        int choice = VerifyInput(menu, 1, 6);
+
+        
+    }
+
+    public static int VerifyInput(string prompt, int start)
+    {
+        bool validInput = false;
+        int input;
+
+        do
+        {
+            input = GetIntInput(prompt);
+
+            if (input >= start)
+            {
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine($"Enter a number larger than or equal to {start}\n");
+            }
+        } while (!validInput);
+        return input;
+    }
+
+    public static int VerifyInput(string prompt, int start, int end)
+    {
+        bool validInput = false;
+        int input;
+
+        do
+        {
+            input = GetIntInput(prompt);
+
+            if (input >= start && input <= end)
+            {
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine($"Enter a number between {start} and {end}\n");
+            }
+        } while (!validInput);
+        return input;
     }
 
     public static int GetIntInput(string prompt)
@@ -42,7 +88,7 @@ internal class Program
     {
         Recipe recipe = new Recipe();
 
-        int numberOfIngredients = GetIntInput("Enter the number of ingredients: ");
+        int numberOfIngredients = VerifyInput("Enter the number of ingredients: ", 1);
         recipe.SetIngredientsLength(numberOfIngredients);
 
         for (int i = 0; i<numberOfIngredients; i++)
@@ -50,7 +96,7 @@ internal class Program
             Console.Write("Name: ");
             string recipeName = Console.ReadLine();
 
-            int recipeQuantity = GetIntInput("Quantity: ");
+            int recipeQuantity = VerifyInput("Quantity: ", 0);
 
             Console.Write("Unit of measurement: ");
             string recipeUnitOfMeasurement = Console.ReadLine();
@@ -58,7 +104,7 @@ internal class Program
         }
 
 
-        int numberOfSteps = GetIntInput("Enter the number of steps: ");
+        int numberOfSteps = VerifyInput("Enter the number of steps: ", 1);
         recipe.SetStepsLength(numberOfSteps);
 
         for (int i = 0; i<numberOfSteps; i++)
