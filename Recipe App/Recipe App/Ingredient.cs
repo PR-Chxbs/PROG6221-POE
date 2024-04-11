35efedb382514bd4e87dc0c7b3709ab86a78602d
Wahina Chabalala <st10310050@rcconnect.edu.za>
2024-04-11 18:24:19 +0200

    Added class attributes to Ingredient class
    Added string name, int quantity, string unitOfMeasurment attributes to the class Ingredient
    
    As well as added the getters and setters for these attributes

--- Recipe App/Recipe App/Ingredient.cs
+++ Recipe App/Recipe App/Ingredient.cs
@@ -2,5 +2,26 @@
 {
     internal class Ingredient
     {
+        private string name;
+        private int quantity;
+        private string unitOfMeasurement;
+
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public string UnitOfMeasurement
+        {
+            get { return unitOfMeasurement; }
+            set { unitOfMeasurement = value; }
+        }
     }
 }
