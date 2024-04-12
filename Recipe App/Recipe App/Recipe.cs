a20b023e60e7bce2cf40215ae7fd7ec34e0a008e
Wahina Chabalala <st10310050@rcconnect.edu.za>
2024-04-12 15:53:41 +0200

    Added comments recipe class

--- Recipe App/Recipe App/Recipe.cs
+++ Recipe App/Recipe App/Recipe.cs
@@ -6,6 +6,7 @@
         private Step[] steps;
         private string recipeName;
 
+        // Method to instantiate an Ingredient
         public void AddIngredient(string name, int quantity, string unitOfMeasurement, int index)
         {
             Ingredient ingredient = new Ingredient
@@ -15,45 +16,54 @@
                 OriginalQuantity = quantity,
                 UnitOfMeasurement = unitOfMeasurement
             };
-            
+
 
             ingredients[index] = ingredient;
         }
 
+        // Instantiate ingredients array
         public void SetIngredientsLength(int length)
         {
             ingredients = new Ingredient[length];
         }
 
+        // Method to instantiate an Step
         public void AddStep(int stepNumber, string name, string description, int index)
         {
-            Step step = new Step(stepNumber);
-            step.StepName = name;
-            step.Description = description;
+            Step step = new Step(stepNumber)
+            {
+                StepName = name,
+                Description = description
+            };
 
             Steps[index] = step;
         }
 
+        // Instantiate steps array
         public void SetStepsLength(int length)
         {
             Steps = new Step[length];
         }
 
+        // Method to display recipe details
         public void PrintDetails()
         {
             string ingredientsString = "";
             string stepString = "";
 
+            // Creating output for ingredient details
             foreach (Ingredient ingredient in ingredients)
             {
                 ingredientsString += $"- {ingredient.Quantity} {ingredient.UnitOfMeasurement} of {ingredient.Name}\n";
             }
 
+            // Creating output for step details
             foreach (Step step in Steps)
             {
                 stepString += $"{step.StepNumber}. {step.Description}\n";
             }
 
+            // Output template
             Console.WriteLine(
                 "==============================================" + "\n" + 
                 "**********************************************" + "\n" +
@@ -67,21 +77,27 @@
                 "==============================================");
         }
 
+        // Method to scale ingredient quantities by a factor
         public void Scale(double factor)
         {
+            // loop to scale for each ingredient
             foreach (Ingredient ingredient in Ingredients)
             {
                 ingredient.Quantity = ingredient.Quantity * factor;
             }
         }
 
+        // Method to reset ingredient quantities back to the original values
         public void ResetIngredientValues()
         {
+            // loop to reset for each ingredient
             foreach (Ingredient ingredient in ingredients)
             {
                 ingredient.ResetQuantity();
             }
         }
+
+        // Method to clear recipe
         public void ClearRecipe()
         {
             ingredients = new Ingredient[0];
@@ -89,6 +105,7 @@
             recipeName = string.Empty;
         }
 
+        // Getters and setters for privare attributes
         public Ingredient[] Ingredients
         {
             get { return ingredients; }
