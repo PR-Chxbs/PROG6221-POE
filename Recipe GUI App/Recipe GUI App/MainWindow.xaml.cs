30cec7a161978b952dfb0d3918e8cdb610ee5ac7
Wahina Chabalala <st10310050@rcconnect.edu.za>
2024-06-08 08:18:24 +0200

    Added MainWindow and back code

--- Recipe GUI App/Recipe GUI App/MainWindow.xaml.cs
+++ Recipe GUI App/Recipe GUI App/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
+﻿using Recipe_GUI_App;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Recipe_GUI_App
+namespace RecipeApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            AddRecipeWindow addRecipeWindow = new AddRecipeWindow();
+            addRecipeWindow.ShowDialog();
+        }
+
+        private void ViewRecipes_Click(object sender, RoutedEventArgs e)
+        {
+            ViewRecipesWindow viewRecipesWindow = new ViewRecipesWindow();
+            viewRecipesWindow.ShowDialog();
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
